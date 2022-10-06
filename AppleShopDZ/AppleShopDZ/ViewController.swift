96918fd302ee86e8e3820e10b514f1221a9743bf
IamTort <61164603+IamTort@users.noreply.github.com>
2022-10-06 11:56:06 +0300

    init

---
+++ AppleShopDZ/AppleShopDZ/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  AppleShopDZ
+//
+//  Created by angelina on 06.10.2022.
+//
+
+import UIKit
+
+/// Контроллер
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
